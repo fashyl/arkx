83216102cdc858bed5cdad2983e3bf16473952c5
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-03-03 13:35:55 +0100

    Configure database connection

---
+++ W05/Day_5/ecommerce-platform/config/database.js
@@ -0,0 +1,42 @@
+// MongoDB's behavior.
+//
+const mongoose = require("mongoose");
+
+// Connect to MongoDB, Local/Atlas depends on the URI
+exports.run = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected to MongoDB.");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB :", error);
+    });
+};
+
+// Tried using mdb, but i'll stick to mongoose for now.
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version (v1)
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// exports.run = async () => {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } catch (error) {
+//     console.log(error); // Timing out but mongoose throws no errors.
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// };
