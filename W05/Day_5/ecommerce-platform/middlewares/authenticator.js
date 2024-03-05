6af9d26e4d3359a08b1d2471370e066cc04ec3e5
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-03-05 12:51:29 +0100

    Add  Middleware for User Authentication and Product's Ownership Verification

---
+++ W05/Day_5/ecommerce-platform/middlewares/authenticator.js
@@ -0,0 +1,34 @@
+const { codes, messages } = require("../config/http");
+const { verify } = require("../helpers/jwt");
+const { Product } = require("../models/schemas/product");
+
+// Authenticate using the created TOKEN KEY
+exports.checkAuthentication = (req, res, next) => {
+  // Check if authorized
+  const token = req.cookies.AUTH_TOKEN || null;
+  if (!token)
+    return res
+      .status(codes.UNAUTHORIZED)
+      .send("Unauthorized. Token not provided.");
+  const decoded = verify(req, res, next, token);
+};
+
+exports.isOwner = async (req, res, next) => {
+  const productId = req.params.id;
+  const product = await Product.findById({ productId });
+  if (!product) return res.status(codes.NOT_FOUND).send("Product not found.");
+  if (product.owner !== req.user.userId) {
+    res
+      .status(codes.FORBIDDEN)
+      .send("Forbidden. You do not have permission to access this resource.");
+  }
+  next();
+};
+
+// Needed once I start rendering login/register pages
+// exports.avoidAuth = (req, res, next) => { // To avoid having to authorize again
+//     const token = req.cookies.AUTH_TOKEN || null;
+//     if (!token) return next(); // solution to jwt must be provided is return
+//     if (!verify(token)) return next();
+//     return res.status(codes.FOUND).redirect("/profile");
+// };
