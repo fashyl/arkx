a17165d24f14ed4a5afe971f3b7deb879710b007
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-11 16:05:16 +0100

    finish kata: penaly for speeding (5kyu)

---
+++ W06/Day_5/kataPenaltyForSpeeding.js
@@ -0,0 +1,47 @@
+/**
+ * You have been speeding on a motorway and a police car had to stop you.
+ * The policeman is a funny guy that likes to play games.
+ * Before issuing penalty charge notice he gives you a choice to change your penalty.
+ *
+ * Your penalty charge is a combination of numbers like:
+ * - speed of your car,
+ * - speed limit in the area,
+ * - speed of the police car chasing you,
+ * - the number of police cars involved, etc.
+ *
+ * So, your task is to combine the given numbers and make the penalty charge to be as small as possible.
+ * For example, if you are given numbers [45, 30, 50, 1] your best choice is 1304550
+ */
+
+// the length of a_list can vary betweem 2 and 20
+function penalty(arr) {
+  for(let w= 0; w < arr.length; w++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i][0] !== arr[i + 1][0]) {
+        if (arr[i][0] > arr[i+1][0]) {
+          swap(arr, i, i + 1);
+        }
+      } else {
+        if (`${arr[i]}${arr[i+1]}` > `${arr[i + 1]}${arr[i]}`) {
+          swap(arr, i, i + 1);
+        }
+      }
+    }
+  }
+  return arr.join('');
+}
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
+}
+
+// console.log(heap(["A", "B", "C"], 3)); // === '1304550');
+// console.log(heap(["A", "B", "C", "D"], 4)); // === '1304550');
+console.log(penalty(["100", "10", "1"])) // === "100101");
+console.log(penalty(["32", "3"])) // === "323");
+console.log(penalty(["70", "46", "4", "19"])) // === "1944670");
+console.log(penalty(["71", "82", "42", "34", "90"])) // === "3442718290");
+console.log(penalty(["31", "97", "6", "78"])) // === "3167897");
